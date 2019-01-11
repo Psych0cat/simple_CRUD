b46c08c158d89cee54bb3d0ef257d6b277910c3b
Alex Nikitin <nikitin.tech@gmail.com>
2019-01-11 11:42:49 +0300

    Heroku config

--- blog_project/wsgi.py
+++ blog_project/wsgi.py
@@ -9,11 +9,10 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+
 from whitenoise.django import DjangoWhiteNoise
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ironman.settings")
-#os.environ("DJANGO_SETTINGS_MODULE", "ironman.settings")
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_p.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
